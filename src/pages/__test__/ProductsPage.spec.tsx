9f0bc2c7422ef42b52ddee28b22bf50f7c461f1f
Juanfi Perez <juanfi@gmail.com>
2024-05-07 14:20:17 +0200

    Add render component for rendering just the tested page

--- src/pages/__test__/ProductsPage.spec.tsx
+++ src/pages/__test__/ProductsPage.spec.tsx
@@ -1,8 +1,8 @@
 import { test } from "vitest";
-import {render, RenderResult, screen} from "@testing-library/react";
-import {ProductsPage} from "../ProductsPage.tsx";
-import {ReactNode} from "react";
-import {AppProvider} from "../../context/AppProvider.tsx";
+import { render, RenderResult, screen } from "@testing-library/react";
+import { ProductsPage } from "../ProductsPage.tsx";
+import { ReactNode } from "react";
+import { AppProvider } from "../../context/AppProvider.tsx";
 
 test("Loads and displays title", async () => {
     renderComponent(<ProductsPage />);
@@ -10,6 +10,6 @@ test("Loads and displays title", async () => {
     await screen.getByRole("heading", { name: "Product price updater" });
 });
 
-function renderComponent (component: ReactNode): RenderResult {
-    return render(<AppProvider>{component}</AppProvider>)
+function renderComponent(component: ReactNode): RenderResult {
+    return render(<AppProvider>{component}</AppProvider>);
 }
